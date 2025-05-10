e8cb9b0334f234ca742dfbdd34f0cd3bc59f5855
Dwarfyyy <106175224+Dwarfyyy@users.noreply.github.com>
2025-05-10 20:56:28 +0500

    Доработка карты + Добавление рестаранов

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { ReviewComponent } from './review-page/review-page/component/reviews.component';
+import { AddRestaurantComponent } from './add-restaurant/add-restaurant.component';
 
 export const routes: Routes = [
   { path: 'reviews', component: ReviewComponent }, // <-- Маршрут для страницы отзывов
+  { path: 'add-restaurant', component: AddRestaurantComponent }, // <-- Маршрут для страницы добавления ресторана
   { path: '', redirectTo: '/reviews', pathMatch: 'full' } // Редирект на /reviews (опционально)
 ];
-
